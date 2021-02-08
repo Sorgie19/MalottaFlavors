f4b52ff6b069d326be27349ec95bc8a452ab1078
RS071903 <Ryan.Sorge@cerner.com>
2021-02-08 16:14:13 -0600

    Initial Commit

---
+++ src/main/java/com/store/malottaflavors/MalottaflavorsApplication.java
@@ -0,0 +1,13 @@
+package com.store.malottaflavors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalottaflavorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalottaflavorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/malottaflavors/MalottaflavorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.malottaflavors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalottaflavorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
