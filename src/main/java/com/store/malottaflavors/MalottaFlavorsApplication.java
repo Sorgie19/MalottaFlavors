f28abae40cd7b6385e4ee450702a5a6842bd83cb
RS071903 <Ryan.Sorge@cerner.com>
2021-02-08 21:31:12 -0600

    Upload spring boot application

---
+++ src/main/java/com/store/malottaflavors/MalottaFlavorsApplication.java
@@ -0,0 +1,13 @@
+package com.store.malottaflavors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalottaFlavorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalottaFlavorsApplication.class, args);
+	}
+
+}
